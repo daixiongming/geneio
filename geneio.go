ce40e52c534f6e1e3f1753e71c2d721fdc7409df
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2016-07-09 19:41:08 -0400

    geneio: define interfaces for gene creation from features

--- geneio.go
+++ geneio.go
@@ -2,19 +2,38 @@
 package geneio
 
 import (
-	"github.com/biogo/biogo/feat/gene"
-
+	"fmt"
 	"io"
+
+	"github.com/biogo/biogo/feat"
+	"github.com/biogo/biogo/feat/gene"
 )
 
-// Reader is the common gene.Interface reader interface.
+const (
+	maxInt = int(^uint(0) >> 1) // The maximum int value.
+)
+
+// FeaturesError describes an error that occurs when a set of features
+// is associated with a gene.
+type FeaturesError struct {
+	Msg   string
+	Gene  gene.Interface
+	Feats []Feature
+}
+
+// Error returns the error message.
+func (e *FeaturesError) Error() string {
+	return fmt.Sprintf("%s for gene %s", e.Msg, e.Gene.Name())
+}
+
+// Reader is the common reader interface for gene.Interface.
 type Reader interface {
 	// Read reads a gene.Interface, returning any error that occurs during the
 	// read.
 	Read() (gene.Interface, error)
 }
 
-// Writer is the common gene.Interface writer interface.
+// Writer is the common writer interface for gene.Interface.
 type Writer interface {
 	// Write writes a gene.Interface, returning the number of bytes written
 	// and any error that occurs during the write.
@@ -22,21 +41,21 @@ type Writer interface {
 }
 
 // Scanner wraps a Reader to provide a convenient loop interface for reading
-// feature data.  Successive calls to the Scan method will step through the
-// genes of the provided Reader. Scanning stops unrecoverably at EOF or the
-// first error.
+// genes. Successive calls to the Scan method will step through the genes of
+// the provided Reader. Scanning stops unrecoverably at EOF or the first
+// error.
 type Scanner struct {
 	r   Reader
 	g   gene.Interface
 	err error
 }
 
-// NewScanner returns a Scanner to read from r.
+// NewScanner returns a new Scanner to read from r.
 func NewScanner(r Reader) *Scanner {
 	return &Scanner{r: r}
 }
 
-// NewScannerFromFunc returns a Scanner to read genes returned by calls to f.
+// NewScannerFromFunc returns a new Scanner to read from calls to f.
 func NewScannerFromFunc(f func() (gene.Interface, error)) *Scanner {
 	return NewScanner(funcReader(f))
 }
@@ -73,4 +92,271 @@ type funcReader func() (gene.Interface, error)
 // Read calls f and returns any gene.Interface and error returned by f.
 func (f funcReader) Read() (gene.Interface, error) {
 	return f()
+}
+
+// Feature is the common interface for features that can be used to construct
+// a Gene.
+type Feature interface {
+	feat.Feature
+	feat.Orienter
+	// GID returns the gene ID of the Feature.
+	GID() string
+	// TID returns the transcript ID of the Feature.
+	TID() string
+	// Type returns the type of the Feature.
+	// e.g. exon, start_codon, stop_codon
+	Type() string
+}
+
+// FeatureReader is the common reader interface for Feature.
+type FeatureReader interface {
+	Read() (Feature, error)
+}
+
+// A GeneReader reads genes from a FeatureReader.
+//
+// It groups features with the same TID and GID into transcripts and genes
+// respectively. It only considers Features of type "exon", "codon_start" and
+// "codon_stop".
+type GeneReader struct {
+	r   FeatureReader
+	blk *geneBlock
+}
+
+// NewGeneReader returns a new GeneReader that reads from r.
+func NewGeneReader(r FeatureReader) *GeneReader {
+	return &GeneReader{r: r}
+}
+
+// Read reads one gene from r. At EOF, it returns nil and io.EOF. When Read
+// returns, r is past the last Feature incorporated in the returned gene.
+func (r *GeneReader) Read() (gene.Interface, error) {
+	for {
+		f, err := r.r.Read()
+		if err != nil {
+			if err == io.EOF && r.blk != nil {
+				g, err := r.blk.ToGene()
+				r.blk = nil
+				return g, err
+			}
+			return nil, err
+		}
+		if r.blk == nil {
+			r.blk = &geneBlock{ID: f.GID(), loc: f.Location(), ori: f.Orientation()}
+			r.blk.feats = append(r.blk.feats, f)
+			continue
+		}
+		if r.blk.ID != f.GID() {
+			g, err := r.blk.ToGene()
+			r.blk = &geneBlock{ID: f.GID(), loc: f.Location(), ori: f.Orientation()}
+			r.blk.feats = append(r.blk.feats, f)
+			return g, err
+		}
+		r.blk.feats = append(r.blk.feats, f)
+	}
+}
+
+// ReadAll reads all the remaining genes from r. A successful call returns err
+// == nil, not err == io.EOF. Because ReadAll is defined to read until EOF, it
+// does not treat end of file as an error to be reported. It returns a nil
+// slice and an error if it encounters one.
+func (r *GeneReader) ReadAll() ([]gene.Interface, error) {
+	var genes []gene.Interface
+	for {
+		g, err := r.Read()
+		if err == io.EOF {
+			return genes, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		genes = append(genes, g)
+	}
+}
+
+// geneBlock wraps a set of Features that correspond to a single gene.
+type geneBlock struct {
+	ID    string
+	loc   feat.Feature
+	ori   feat.Orientation
+	feats []Feature
+}
+
+// ToGene creates and returns a gene. It also creates the transcripts
+// associated with the gene. It returns nil and an error if it encounters one.
+func (geneBlk *geneBlock) ToGene() (*gene.Gene, error) {
+	g := &gene.Gene{
+		ID:     geneBlk.ID,
+		Orient: geneBlk.ori,
+		Chrom:  geneBlk.loc,
+		Offset: maxInt, // is rewritten later to get the minimum start.
+	}
+
+	// Find proper gene offset and check that all features are on the same
+	// location and orientation.
+	for _, f := range geneBlk.feats {
+		if f.Start() < g.Offset {
+			g.Offset = f.Start()
+		}
+		if f.Orientation() != g.Orient {
+			return nil, &FeaturesError{
+				Gene:  g,
+				Msg:   "geneio: features with varying orientation",
+				Feats: geneBlk.feats,
+			}
+		}
+		if f.Location() != g.Chrom {
+			return nil, &FeaturesError{
+				Gene:  g,
+				Msg:   "geneio: features on varying location",
+				Feats: geneBlk.feats,
+			}
+		}
+	}
+
+	// Build the transcripts.
+	var features []feat.Feature
+	var i, j int
+	for i < len(geneBlk.feats) {
+		tid := geneBlk.feats[i].TID()
+		for j = i + 1; j < len(geneBlk.feats); j++ {
+			if geneBlk.feats[j].TID() != tid {
+				break
+			}
+		}
+		tr, err := newTrancript(g, tid, geneBlk.feats[i:j])
+		if err != nil {
+			return nil, err
+		}
+		features = append(features, tr)
+		i = j
+	}
+
+	// Attach the transcripts to the gene.
+	if err := g.SetFeatures(features...); err != nil {
+		return nil, err
+	}
+
+	return g, nil
+}
+
+// newTrancript creates and returns a new gene.Transcript from s. Returns nil
+// and the error if it encounters one.
+func newTrancript(g *gene.Gene, tid string, s []Feature) (gene.Transcript, error) {
+	for _, f := range s {
+		if f.Type() == "start_codon" {
+			return newCodingTranscript(g, tid, s)
+		}
+	}
+	return newNonCodingTranscript(g, tid, s)
+}
+
+// newCodingTranscript creates and returns a new gene.CodingTranscript from s.
+// Returns nil and an error if it encounters one.
+func newCodingTranscript(
+	g *gene.Gene, tid string, s []Feature) (*gene.CodingTranscript, error) {
+
+	t := &gene.CodingTranscript{ID: tid, Loc: g, Orient: feat.Forward}
+
+	// Find offset.
+	minStart := maxInt
+	for _, f := range s {
+		if f.Start() < minStart {
+			minStart = f.Start()
+		}
+	}
+	t.Offset = minStart - g.Offset
+
+	// Parse Features.
+	var exons []gene.Exon
+	for _, f := range s {
+		switch f.Type() {
+		case "exon":
+			e := gene.Exon{
+				Transcript: t,
+				Offset:     f.Start() - t.Offset - g.Offset,
+				Length:     f.End() - f.Start(),
+			}
+			exons = append(exons, e)
+		case "start_codon":
+			if f.Orientation() == feat.Forward {
+				t.CDSstart = f.Start() - t.Offset - g.Offset
+			} else if f.Orientation() == feat.Reverse {
+				t.CDSend = f.End() - t.Offset - g.Offset
+			}
+		case "stop_codon":
+			if f.Orientation() == feat.Forward {
+				t.CDSend = f.End() - t.Offset - g.Offset
+			} else if f.Orientation() == feat.Reverse {
+				t.CDSstart = f.Start() - t.Offset - g.Offset
+			}
+		}
+	}
+
+	// Set the transcript exons.
+	exons = mergeExons(exons)
+	if err := t.SetExons(exons...); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// newNonCodingTranscript creates and returns a new gene.NonCodingTranscript
+// from s. Returns nil and an error if it encounters one.
+func newNonCodingTranscript(
+	g *gene.Gene, tid string, s []Feature) (*gene.NonCodingTranscript, error) {
+
+	t := &gene.NonCodingTranscript{ID: tid, Loc: g, Orient: feat.Forward}
+
+	// Find offset.
+	minStart := maxInt
+	for _, f := range s {
+		if f.Start() < minStart {
+			minStart = f.Start()
+		}
+	}
+	t.Offset = minStart - g.Offset
+
+	// Parse Features.
+	var exons []gene.Exon
+	for _, f := range s {
+		if f.Type() == "exon" {
+			e := gene.Exon{
+				Transcript: t,
+				Offset:     f.Start() - t.Offset - g.Offset,
+				Length:     f.End() - f.Start(),
+			}
+			exons = append(exons, e)
+		}
+	}
+
+	// Set the transcript exons.
+	exons = mergeExons(exons)
+	if err := t.SetExons(exons...); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// mergeExons returns a slice with touching exons concatenated into one.
+// Touching exons are those that one's start equals the other's end.
+func mergeExons(exons []gene.Exon) []gene.Exon {
+	if len(exons) < 2 {
+		return exons
+	}
+	var merged []gene.Exon
+	e, exons := exons[0], exons[1:]
+	merged = append(merged, e)
+	for len(exons) > 0 {
+		e, exons = exons[0], exons[1:]
+		if merged[len(merged)-1].End() == e.Start() {
+			merged[len(merged)-1].Length = merged[len(merged)-1].Len() + e.Len()
+		} else {
+			merged = append(merged, e)
+		}
+	}
+
+	return merged
 }
--- geneio_test.go
+++ geneio_test.go
@@ -3,22 +3,88 @@ package geneio
 import (
 	"errors"
 	"io"
+	"reflect"
+	"sort"
+	"strings"
 	"testing"
 
+	"github.com/biogo/biogo/feat"
 	"github.com/biogo/biogo/feat/gene"
-
-	"gopkg.in/check.v1"
+	"github.com/biogo/biogo/io/featio/gff"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { check.TestingT(t) }
+// Assert that interfaces are satisfied.
+var (
+	_ Reader        = (*GeneReader)(nil)
+	_ FeatureReader = (*FeatureReaderImpl)(nil)
+)
 
-// Create the test suite
-type S struct{}
+// Define this type to enable sorting and make the tests stable.
+type Genes []gene.Interface
 
-var _ = check.Suite(&S{})
+func (s Genes) Len() int {
+	return len(s)
+}
 
-func geneCreator(g gene.Interface, err error) func() (gene.Interface, error) {
+func (s Genes) Less(i, j int) bool {
+	return s[i].Name() < s[j].Name()
+}
+
+func (s Genes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// FeatureReaderImpl is a simple implementation of FeatureReader.
+type FeatureReaderImpl struct {
+	r        *gff.Reader
+	GID, TID string
+}
+
+func (r *FeatureReaderImpl) Read() (Feature, error) {
+	f, err := r.r.Read()
+	if err != nil {
+		return nil, err
+	}
+	return r.NewFeatureImpl(f)
+}
+
+func (r *FeatureReaderImpl) NewFeatureImpl(f feat.Feature) (*FeatureImpl, error) {
+	gf, ok := f.(*gff.Feature)
+	if !ok {
+		return nil, errors.New("gff: feature is not a GFF feature")
+	}
+	gid := gf.FeatAttributes.Get(r.GID)
+	if gid == "" {
+		return nil, errors.New("gff: empty grouping " + r.GID + " field")
+	}
+	tid := gf.FeatAttributes.Get(r.TID)
+	if tid == "" {
+		return nil, errors.New("gff: empty grouping " + r.TID + " field")
+	}
+	return &FeatureImpl{
+		Feature: f,
+		fgid:    gid,
+		ftid:    tid,
+		ftype:   gf.Feature,
+		ori:     feat.Orientation(gf.FeatStrand),
+	}, nil
+}
+
+// FeatureImpl is a simple implementation of Feature.
+type FeatureImpl struct {
+	feat.Feature
+	ori               feat.Orientation
+	fgid, ftid, ftype string
+}
+
+func (f *FeatureImpl) GID() string                   { return f.fgid }
+func (f *FeatureImpl) TID() string                   { return f.ftid }
+func (f *FeatureImpl) Type() string                  { return f.ftype }
+func (f *FeatureImpl) Orientation() feat.Orientation { return f.ori }
+
+// geneGenerator is the simplest gene generator. It just returns the gene that
+// it gets.
+func geneGenerator(g gene.Interface, err error) func() (gene.Interface, error) {
 	return func() (gene.Interface, error) {
 		tmpG, tmpE := g, err
 		g, err = nil, io.EOF
@@ -28,28 +94,29 @@ func geneCreator(g gene.Interface, err error) func() (gene.Interface, error) {
 
 // Test ReadFromFunc
 var readFromFuncTests = []struct {
-	f   func() (gene.Interface, error)
-	g   gene.Interface
-	err string
+	name string
+	f    func() (gene.Interface, error)
+	g    gene.Interface
+	err  string
 }{
 	{
-		geneCreator(&gene.Gene{ID: "A"}, nil),
-		&gene.Gene{ID: "A"},
-		"",
+		f:   geneGenerator(&gene.Gene{ID: "A"}, nil),
+		g:   &gene.Gene{ID: "A"},
+		err: "",
 	},
 	{
-		geneCreator(nil, errors.New("returning error")),
-		nil,
-		"returning error",
+		f:   geneGenerator(nil, errors.New("returning error")),
+		g:   nil,
+		err: "returning error",
 	},
 	{
-		geneCreator(&gene.Gene{ID: "B"}, io.EOF),
-		&gene.Gene{ID: "B"},
-		"",
+		f:   geneGenerator(&gene.Gene{ID: "B"}, io.EOF),
+		g:   &gene.Gene{ID: "B"},
+		err: "",
 	},
 }
 
-func (s *S) TestReadFromFunc(c *check.C) {
+func TestReadFromFunc(t *testing.T) {
 	for _, tt := range readFromFuncTests {
 		sc := NewScannerFromFunc(tt.f)
 
@@ -57,19 +124,240 @@ func (s *S) TestReadFromFunc(c *check.C) {
 		g, err := sc.Gene(), sc.Error()
 
 		// Next() should not return true when there is an error.
-		if ok {
-			c.Assert(err, check.Equals, nil)
+		if ok && err != nil {
+			t.Errorf("%s: unexpected error %v", tt.name, err)
 		}
 
 		if tt.err != "" {
-			c.Assert(err, check.ErrorMatches, tt.err)
+			if err == nil || !strings.Contains(err.Error(), tt.err) {
+				t.Errorf("%s: error %q, want error %q", tt.name, err, tt.err)
+			}
+			continue
 		} else if err != nil {
-			c.Errorf("Unexpected error %s", err)
+			t.Errorf("%s: unexpected error %v", tt.name, err)
+			continue
 		}
 
-		c.Check(g, check.DeepEquals, tt.g)
+		if !reflect.DeepEqual(g, tt.g) {
+			t.Errorf("%s: out gene=%v want %v", tt.name, g, tt.g)
+		}
 
 		// Second call to Next() should return false.
-		c.Assert(sc.Next(), check.Equals, false)
+		if sc.Next() {
+			t.Errorf("last cast call to next returned true instead of false")
+		}
+	}
+}
+
+// Test GeneReader's Read
+var readTests = []struct {
+	Name             string
+	Input            string
+	Sorted           bool
+	Error            string
+	GeneCnt, FeatCnt int
+	IDs, Chrs        []string
+	Starts, Ends     []int
+	Orientations     []feat.Orientation
+}{
+	{
+		Name: "Mixed sorted",
+		Input: "" +
+			"X\t.\texon\t10\t20\t0\t+\t.\tgene_id A; transcript_id A1;\n" +
+			"X\t.\texon\t15\t30\t0\t+\t.\tgene_id A; transcript_id A2;\n" +
+			"X\t.\texon\t20\t45\t0\t+\t.\tgene_id A; transcript_id A3;\n" +
+			"X\t.\texon\t25\t30\t0\t+\t.\tgene_id A; transcript_id A4;\n" +
+			"Y\t.\texon\t50\t90\t0\t-\t.\tgene_id B; transcript_id B1;\n" +
+			"Y\t.\tfoo\t100\t200\t0\t-\t.\tgene_id B; transcript_id B1;\n" +
+			"Y\t.\tstop_codon\t60\t62\t0\t-\t.\tgene_id B; transcript_id B1;\n" +
+			"Y\t.\tstart_codon\t71\t73\t0\t-\t.\tgene_id B; transcript_id B1;\n",
+		Sorted:       true,
+		GeneCnt:      2,
+		FeatCnt:      5,
+		IDs:          []string{"A", "B"},
+		Chrs:         []string{"X", "Y"},
+		Starts:       []int{9, 49},
+		Ends:         []int{45, 90},
+		Orientations: []feat.Orientation{feat.Forward, feat.Reverse},
+	},
+	{
+		Name: "Single forward coding gene",
+		Input: "" +
+			"X\t.\texon\t2\t70\t0\t+\t.\tgene_id C; transcript_id C1;\n" +
+			"X\t.\texon\t80\t90\t0\t+\t.\tgene_id C; transcript_id C1;\n" +
+			"X\t.\tstart_codon\t60\t62\t0\t+\t.\tgene_id C; transcript_id C1;\n" +
+			"X\t.\tstop_codon\t81\t83\t0\t+\t.\tgene_id C; transcript_id C1;\n",
+		Sorted:       true,
+		GeneCnt:      1,
+		FeatCnt:      1,
+		IDs:          []string{"C"},
+		Chrs:         []string{"X"},
+		Starts:       []int{1},
+		Ends:         []int{90},
+		Orientations: []feat.Orientation{feat.Forward},
+	},
+	{
+		Name: "Single reverse coding gene",
+		Input: "" +
+			"X\t.\texon\t30\t50\t0\t-\t.\tgene_id D; transcript_id D1;\n" +
+			"X\t.\texon\t80\t99\t0\t-\t.\tgene_id D; transcript_id D1;\n" +
+			"X\t.\tstop_codon\t40\t42\t0\t-\t.\tgene_id D; transcript_id D1;\n" +
+			"X\t.\tstart_codon\t91\t93\t0\t-\t.\tgene_id D; transcript_id D1;\n",
+		Sorted:       true,
+		GeneCnt:      1,
+		FeatCnt:      1,
+		IDs:          []string{"D"},
+		Chrs:         []string{"X"},
+		Starts:       []int{29},
+		Ends:         []int{99},
+		Orientations: []feat.Orientation{feat.Reverse},
+	},
+	{
+		Name:   "Missing gene_id",
+		Input:  "X\t.\texon\t30\t50\t0\t-\t.\ttranscript_id D1;\n",
+		Sorted: true,
+		Error:  "gff: empty grouping gene_id field",
+	},
+	{
+		Name:   "Missing transcript_id",
+		Input:  "X\t.\texon\t30\t50\t0\t-\t.\tgene_id D;\n",
+		Sorted: true,
+		Error:  "gff: empty grouping transcript_id field",
+	},
+	{
+		Name: "Features on different location",
+		Input: "" +
+			"X\t.\texon\t30\t50\t0\t-\t.\tgene_id F; transcript_id F1;\n" +
+			"Y\t.\texon\t80\t99\t0\t-\t.\tgene_id F; transcript_id F1;\n",
+		Sorted: true,
+		Error:  "geneio: features on varying location for gene F",
+	},
+	{
+		Name: "Features on different orientation",
+		Input: "" +
+			"X\t.\texon\t30\t50\t0\t-\t.\tgene_id G; transcript_id G1;\n" +
+			"X\t.\texon\t80\t99\t0\t+\t.\tgene_id G; transcript_id G1;\n",
+		Sorted: true,
+		Error:  "geneio: features with varying orientation for gene G",
+	},
+	{
+		Name: "Single non coding gene with adjacent exons",
+		Input: "" +
+			"X\t.\texon\t30\t50\t0\t-\t.\tgene_id H; transcript_id H1;\n" +
+			"X\t.\texon\t51\t99\t0\t-\t.\tgene_id H; transcript_id H1;\n",
+		Sorted:       true,
+		GeneCnt:      1,
+		FeatCnt:      1,
+		IDs:          []string{"H"},
+		Chrs:         []string{"X"},
+		Starts:       []int{29},
+		Ends:         []int{99},
+		Orientations: []feat.Orientation{feat.Reverse},
+	},
+	{
+		Name: "Single non coding gene with overlapping exons",
+		Input: "" +
+			"X\t.\texon\t2\t70\t0\t+\t.\tgene_id F; transcript_id F1;\n" +
+			"X\t.\texon\t70\t90\t0\t+\t.\tgene_id F; transcript_id F1;\n",
+		Sorted: true,
+		Error:  "exons overlap",
+	},
+	{
+		Name: "Single coding gene with adjacent exons",
+		Input: "" +
+			"X\t.\texon\t10\t100\t0\t-\t.\tgene_id I; transcript_id I1;\n" +
+			"X\t.\tstop_codon\t40\t42\t0\t-\t.\tgene_id I; transcript_id I1;\n" +
+			"X\t.\tstart_codon\t91\t93\t0\t-\t.\tgene_id I; transcript_id I1;\n",
+		Sorted:       true,
+		GeneCnt:      1,
+		FeatCnt:      1,
+		IDs:          []string{"I"},
+		Chrs:         []string{"X"},
+		Starts:       []int{9},
+		Ends:         []int{100},
+		Orientations: []feat.Orientation{feat.Reverse},
+	},
+	{
+		Name: "Single coding gene with overlapping exons",
+		Input: "" +
+			"X\t.\texon\t2\t70\t0\t+\t.\tgene_id F; transcript_id F1;\n" +
+			"X\t.\texon\t70\t90\t0\t+\t.\tgene_id F; transcript_id F1;\n" +
+			"X\t.\tstart_codon\t60\t62\t0\t+\t.\tgene_id F; transcript_id F1;\n" +
+			"X\t.\tstop_codon\t81\t83\t0\t+\t.\tgene_id F; transcript_id F1;\n",
+		Sorted: true,
+		Error:  "exons overlap",
+	},
+}
+
+func TestGeneReaderRead(t *testing.T) {
+	for _, tt := range readTests {
+		r := GeneReader{
+			r: &FeatureReaderImpl{
+				r:   gff.NewReader(strings.NewReader(tt.Input)),
+				GID: "gene_id",
+				TID: "transcript_id",
+			},
+		}
+
+		genes, err := r.ReadAll()
+		if tt.Error != "" {
+			if err == nil || !strings.Contains(err.Error(), tt.Error) {
+				t.Errorf("%s: error %q, want error %q", tt.Name, err, tt.Error)
+			}
+			continue
+		} else if err != nil {
+			t.Errorf("%s: unexpected error %v", tt.Name, err)
+			continue
+		}
+		genesSorted := Genes(genes)
+		sort.Sort(genesSorted)
+
+		var feats []feat.Feature
+		var ids, chrs []string
+		var starts, ends []int
+		var orientations []feat.Orientation
+		for _, g := range genesSorted {
+			feats = append(feats, g.Features()...)
+			ids = append(ids, g.Name())
+			starts = append(starts, g.Start())
+			ends = append(ends, g.End())
+			chrs = append(chrs, g.Location().Name())
+			orientations = append(orientations, g.Orientation())
+		}
+		if len(genes) != tt.GeneCnt {
+			t.Errorf("%s: out gene count=%d want %d", tt.Name, len(genes), tt.GeneCnt)
+		}
+		if len(feats) != tt.FeatCnt {
+			t.Errorf("%s: out feat count=%d want %d", tt.Name, len(feats), tt.FeatCnt)
+		}
+		if !reflect.DeepEqual(ids, tt.IDs) {
+			t.Errorf("%s: out ids=%q want %q", tt.Name, ids, tt.IDs)
+		}
+		if !reflect.DeepEqual(starts, tt.Starts) {
+			t.Errorf("%s: out starts=%d want %d", tt.Name, starts, tt.Starts)
+		}
+		if !reflect.DeepEqual(ends, tt.Ends) {
+			t.Errorf("%s: out ends=%d want %d", tt.Name, ends, tt.Ends)
+		}
+		if !reflect.DeepEqual(chrs, tt.Chrs) {
+			t.Errorf("%s: out chrs=%q want %q", tt.Name, chrs, tt.Chrs)
+		}
+		if !reflect.DeepEqual(orientations, tt.Orientations) {
+			t.Errorf("%s: out orientations=%q want %q", tt.Name, orientations, tt.Orientations)
+		}
+	}
+}
+
+func BenchmarkGeneReaderRead(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		tt := readTests[0]
+		r := GeneReader{
+			r: &FeatureReaderImpl{
+				r:   gff.NewReader(strings.NewReader(tt.Input)),
+				GID: "gene_id",
+				TID: "transcript_id",
+			},
+		}
+		_, _ = r.ReadAll()
 	}
 }
